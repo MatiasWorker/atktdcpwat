b07088041e9d857fc3827dd67a2c345c1b230ef0
uppercod <uppercod.git@gmail.com>
2023-03-16 11:03:51 -0300

    CDN

--- build.mjs
+++ build.mjs
@@ -3,4 +3,4 @@ import { writeFile } from "fs/promises";
 
 const result = sass.compile("./src/styles/main.scss", {});
 
-await writeFile("./public/web-component.css", result.css, "utf8");
+await writeFile("./cdn/web-component.css", result.css, "utf8");
